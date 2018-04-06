c6abd5201660c819d32ca950f28ee56afde21cf6
acer-PC <540740324@qq.com>
2018-04-06 16:08:51 +0800

    添加资源

--- app/src/main/java/com/inter/ui/MainActivity.java
+++ app/src/main/java/com/inter/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.inter;
+package com.inter.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.inter.R;
 
 public class MainActivity extends AppCompatActivity {
 
